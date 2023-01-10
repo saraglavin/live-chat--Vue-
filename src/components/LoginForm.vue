e0db48e4660c27fc2454f03e169e6f1b2e112fe7
Sara Glavin <hej@saraglavin.se>
2023-01-10 12:43:05 +0100

    Added Login and Signupforms

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" required placeholder="Email" v-model="email">
+    <input type="password" required placeholder="Password" v-model="password">
+    <button>Log In</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    setup() {
+        //refs
+        const email = ref('')
+        const password = ref('')
+
+        const handleSubmit = () => {
+            console.log(email.value, password.value);
+        }
+
+
+        return { email, password, handleSubmit }
+    }
+}
+</script>
+
+<style>
+
+</style>
