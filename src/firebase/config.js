fc661cd33b002cdfa176f51d9e2570d760100eac
Sara Glavin <hej@saraglavin.se>
2023-01-10 09:35:04 +0100

    Added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBUzT665ifGeYHh1ADRdAYrskioPC1Hijc",
+    authDomain: "udemy-vue-firebase-24898.firebaseapp.com",
+    projectId: "udemy-vue-firebase-24898",
+    storageBucket: "udemy-vue-firebase-24898.appspot.com",
+    messagingSenderId: "1068229868677",
+    appId: "1:1068229868677:web:dcb32367a1f861fd627dec"
+  };
+
+  // init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  const projectFirestore = firebase.firestore()
+
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+  export { projectFirestore, timestamp }
