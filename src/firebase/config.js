7ef9f8a785a60d6ceda852fb1870ced90e4892b6
Sara Glavin <hej@saraglavin.se>
2023-01-11 14:56:39 +0100

    Creating Signup logic

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBUzT665ifGeYHh1ADRdAYrskioPC1Hijc",
@@ -13,8 +14,8 @@ const firebaseConfig = {
   // init firebase
   firebase.initializeApp(firebaseConfig)
 
+  const projectAuth = firebase.auth()
   const projectFirestore = firebase.firestore()
-
   const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-  export { projectFirestore, timestamp }
+  export { projectFirestore, projectAuth, timestamp }
