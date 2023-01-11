7d97de15871f3b402a187e8b46af8f8e92dcfed8
Sara Glavin <hej@saraglavin.se>
2023-01-11 16:18:27 +0100

    Added Navbar and log out functionality

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <div>
+        <p>Hey there, displayname</p>
+        <p class="email"> Currently logged in as EMAIL</p>
+    </div>
+    <button @click="handleClick">Log Out</button>
+  </nav>
+</template>
+
+<script>
+import useLogout from '../composables/useLogout'
+
+export default {
+ setup() {
+  const { logout , error } = useLogout()
+
+  const handleClick = async () => {
+      await logout()
+      if(!error.value) {
+        console.log('User logged out');
+      }
+  }
+
+  return { handleClick , error }
+ }
+}
+</script>
+
+<style>
+
+</style>
