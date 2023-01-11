68371e99d9d87640864deb1827bd6c4dee642a23
Sara Glavin <hej@saraglavin.se>
2023-01-11 15:14:31 +0100

    Added login functionality

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -3,6 +3,7 @@
     <input type="text" required placeholder="Display name" v-model="displayName">
     <input type="email" required placeholder="Email" v-model="email">
     <input type="password" required placeholder="Password" v-model="password">
+    <div class="error">{{ error }}</div>
     <button>Sign up</button>
   </form>
 </template>
@@ -27,7 +28,7 @@ export default {
         }
 
 
-        return { displayName, email, password, handleSubmit }
+        return { displayName, email, password, handleSubmit, error }
     }
 }
 </script>
