d4c2954dff929e192288d1ddc9e60215d80d864c
Sara Glavin <hej@saraglavin.se>
2023-01-12 11:29:09 +0100

    Redirect when logged out

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -7,8 +7,24 @@
 
 <script>
 import Navbar from '../components/Navbar.vue'
+import getUser from '../composables/getUser'
+import { useRouter } from 'vue-router'
+import { watch } from '@vue/runtime-core'
+
 export default {
-    components: { Navbar }
+    components: { Navbar },
+    setup() {
+    const { user } = getUser()
+    const router = useRouter()
+
+    watch(user, () => {
+        if(!user.value) {
+            router.push({ name: 'Welcome' })
+        }
+    })
+
+
+    }
 }
 </script>
 
