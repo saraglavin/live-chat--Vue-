a7d61e84cc6f02e9cd4c2e6eddfdc175328b8224
Sara Glavin <hej@saraglavin.se>
2023-01-13 10:24:25 +0100

    Added formatting on time

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -2,9 +2,9 @@
   <div class="chat-window">
     <div v-if="error"> {{ error }}</div>
     <div v-if="documents" class="messages">
-        <div v-for="doc in documents" :key="doc.id" class="single">
+        <div v-for="doc in formattedDocuments" :key="doc.id" class="single">
             <span class="created-at">
-                {{ doc.createdAt.toDate() }}
+                {{ doc.createdAt }}
             </span>
             <span class="name">
                 {{ doc.name }}
@@ -19,13 +19,23 @@
 
 <script>
 import getCollection from '../composables/getCollection'
+import { formatDistanceToNow } from 'date-fns'
+import { computed } from '@vue/runtime-core'
 
 export default {
     setup() {
         const { error, documents } = getCollection('messages')
 
+        const formattedDocuments = computed(() => {
+            if(documents.value) {
+                return documents.value.map(doc => {
+                    let time = formatDistanceToNow(doc.createdAt.toDate())
+                    return { ...doc, createdAt: time }
+                })
+            }
+        })
 
-        return { error, documents }
+        return { error, documents, formattedDocuments }
     }
 }
 </script>
